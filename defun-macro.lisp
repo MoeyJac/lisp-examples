a6d2f73a16320f49ea8c0f3223ced92a0170314a
moeyJac <jo3ymac@gmail.com>
2025-07-11 02:58:48 -0400

    Adds utility macros and functions.
    Introduces several Lisp macros and functions for conditional execution and code generation.
    These include `if-let` for conditional variable binding, `if-swap` for inverting conditional branches, and `my-and` for short-circuiting logical conjunction, as well as helper functions.
    
    The code also includes examples and comments to illustrate their usage and purpose.

--- defun-macro.lisp
+++ defun-macro.lisp
@@ -23,3 +23,34 @@
 
 (defmacro if-swap (stmt false-expr true-expr)
     `(if ,stmt ,true-expr ,false-expr))
+
+(defun my-when (condition true-fn)
+    (if condition (funcall true-fn)))
+
+; (my-when (< 41 42)
+;         (lambda ()
+;           (princ "Hello")
+;           (princ "World")
+;         )
+; )
+
+(defun foof () 42)
+(defmacro foom () 42)
+
+; (defmacro my-and (&rest x)
+;     (if (= (length x) 1) 
+;             (first x) 
+;             `(if ,(first x) 
+;                 ,(first x) 
+;                 (my-and ,@(cdr x))
+;             )
+;     )
+; )
+
+(defmacro my-and (&rest x)
+    (cond
+        ((null x)   'nil)
+        ((first x)  (first x))
+        (t          `(my-and ,@(cdr x)))
+    ) 
+)
